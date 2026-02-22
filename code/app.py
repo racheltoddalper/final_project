f7b45ba7fef95cdc86c5d894af9d15a466ada09a
racheltoddalper <rachelalper@uchicago.edu>
2026-02-22 14:43:45 -0600

    inital streamlit app

---
+++ code/app.py
@@ -0,0 +1,6 @@
+# Dashboard code will go here. We haven't covered this yet.
+
+# ideas:
+# some sort of plot of income and number of violations or seasonal number of violations but you can toggle it by type of violation
+# seasonal trends map where you toggle the months to see which violation types are the most common
+# or select a violation type and then toggle for seasonal trends
